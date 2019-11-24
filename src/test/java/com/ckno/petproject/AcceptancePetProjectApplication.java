286785a5a73c322d03b632136aa07ada4ecbf770
carloscano93 <carlcan.93@gmail.com>
2019-11-24 21:26:02 +0100

    better acceptance test

--- src/test/java/com/ckno/petproject/AcceptancePetProjectApplication.java
+++ src/test/java/com/ckno/petproject/AcceptancePetProjectApplication.java
@@ -5,13 +5,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,10 +22,12 @@ class AcceptancePetProjectApplication {
 
     @Test
     public void say_hello_pet() throws Exception {
-        this.mockMvc.perform(get("/v1/")
-                .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json;charset=UTF-8"));
+        this.mockMvc.perform(get("/v1/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString()
+                .equals("Hello Pet");
     }
 
     @Test
