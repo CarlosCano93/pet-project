c61ee59a2f3a204f0b8b7dfa7a9a67aa9d13106b
carloscano93 <carlcan.93@gmail.com>
2021-03-27 17:16:56 +0100

    allow origins

--- src/main/java/com/ckno/petproject/api/FiaIGpController.java
+++ src/main/java/com/ckno/petproject/api/FiaIGpController.java
@@ -2,6 +2,7 @@ package com.ckno.petproject.api;
 
 import com.ckno.petproject.adapters.igp.IGpRepository;
 import com.ckno.petproject.adapters.igp.dto.ProDriver;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class FiaIGpController {
         this.iGpRepository = iGpRepository;
     }
 
+    @CrossOrigin(origins = "https://campeonato-fia-igp.herokuapp.com/")
     @GetMapping("pro-drivers")
     public List<ProDriver> getProDrivers() {
         return iGpRepository.findAll();
