cf0d4d71939f4899ac11d64e1b01c696e0e78fcf
carloscano93 <carlcan.93@gmail.com>
2021-03-29 22:50:59 +0200

    fix cors

--- src/main/java/com/ckno/petproject/WebConfiguration.java
+++ src/main/java/com/ckno/petproject/WebConfiguration.java
@@ -1,5 +1,7 @@
 package com.ckno.petproject;
 
+import io.sentry.Sentry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,15 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
+@Slf4j
 public class WebConfiguration implements WebMvcConfigurer {
 
-    @Value("${com.ckno.igp-front-url}")
-    private String campeonatoFiaUrl;
+    @Value("${com.ckno.igp-front-url}") private String campeonatoFiaUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        Sentry.capture("campeonatoFiaUrl: " + campeonatoFiaUrl);
         registry.addMapping("/v1/igp/**")
-                .allowedOrigins(campeonatoFiaUrl)
+                .allowedOrigins("https://campeonato-fia-igp.herokuapp.com")
                 .allowedMethods("GET");
     }
 }
