1310908c1e36c50363ca5bfc821c36c2974c7ac7
CarlosCano93 <carlos.cano@clearpay.com>
2021-10-01 12:10:57 +0200

    fresh start with java 17 and spring-boot 2.6.0

--- src/main/java/com/ckno/petproject/adapters/igp/IGpRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ckno.petproject.adapters.igp;
-
-import com.ckno.petproject.adapters.igp.dto.Driver;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IGpRepository extends MongoRepository<Driver, String> {
-}
