5ad87f2d786c112a514debb18151af88b1c8766f
CarlosCano93 <carlos.cano@clearpay.com>
2021-10-01 16:21:56 +0200

    integration with an pokeAPI in one file

--- src/main/java/com/ckno/petproject/PetProjectApplication.java
+++ src/main/java/com/ckno/petproject/PetProjectApplication.java
@@ -1,8 +1,24 @@
 package com.ckno.petproject;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.List;
+
+@EnableFeignClients
 @SpringBootApplication
 public class PetProjectApplication {
 
---
+++ src/test/java/com/ckno/petproject/PetProjectApplicationTests.java
@@ -0,0 +1,52 @@
+package com.ckno.petproject;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class PetProjectApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void say_hello() throws Exception {
+        mockMvc.perform(get("/v1/pokemon/hello/{name}", "ditto"))
+               .andExpect(status().is2xxSuccessful())
+               .andExpect(content().string("Hello ditto!"));
+    }
+
+    @Test
+    void return_200_if_pokemon_exists() throws Exception {
+        mockMvc.perform(get("/v1/pokemon/exists/{name}", "charmander"))
+               .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    void return_404_if_pokemon_does_not_exists() throws Exception {
+        mockMvc.perform(get("/v1/pokemon/exists/{name}", "digimon"))
+               .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void return_pokemon_if_exists() throws Exception {
+        String jsonResponse = Map.of("name", "charmander",
+                                     "type", "fire")
+                                 .toString();
+
+        mockMvc.perform(get("/v1/pokemon/{name}", "charmander"))
+               .andExpect(status().is2xxSuccessful())
+               .andExpect(content().contentType(jsonResponse));
+    }
+}
