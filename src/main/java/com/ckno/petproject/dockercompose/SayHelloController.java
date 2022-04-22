c87619272c05bebf8f123df70f8e17cbbc7f0de7
Carlos Cano <ccano@squareup.com>
2022-04-22 13:31:35 +0200

    fix(): docker compose working with mongo

--- src/main/java/com/ckno/petproject/dockercompose/SayHelloController.java
+++ src/main/java/com/ckno/petproject/dockercompose/SayHelloController.java
@@ -1,24 +1,37 @@
 package com.ckno.petproject.dockercompose;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping("v1/hello")
 @RequiredArgsConstructor
+@Log4j2
 public class SayHelloController {
 
     private final NamesRepository namesRepository;
 
     @GetMapping
     String randomHello() {
-        return "Hey " + namesRepository.findAll().get(0);
+        List<Person> persons = namesRepository.findAll();
+        Random random = new Random();
+        return "Hey " + persons.get(random.nextInt(persons.size())).name();
     }
 
-    @GetMapping("/add")
-    void addName() {
-        namesRepository.save(new Person("Carlos"));
+    @GetMapping("/add/{name}")
+    void addName(@PathVariable String name) {
+        namesRepository.save(new Person(name));
+    }
+
+    @GetMapping("/all")
+    List<Person> all() {
+        return namesRepository.findAll();
     }
 }
